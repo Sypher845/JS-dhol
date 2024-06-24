24617faded8d0cfa7779ed28de53622ab2993e8e
Sypher845 <162715611+Sypher845@users.noreply.github.com>
2024-06-24 12:38:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+function wordChecker(buttonInnerHTML)
+{
+    var audio;    
+    switch (buttonInnerHTML) {
+        case "w":
+            audio= new Audio("./sounds/crash.mp3");
+            audio.play();                   
+            break;
+        case "a":
+            audio= new Audio("./sounds/kick-bass.mp3");
+            audio.play();                   
+            break;
+        case "s":
+            audio= new Audio("./sounds/snare.mp3");
+            audio.play();                   
+            break;
+        case "d":
+            audio= new Audio("./sounds/tom-1.mp3");
+            audio.play();                   
+            break;
+        case "j":
+            audio= new Audio("./sounds/tom-2.mp3");
+            audio.play();                   
+            break; 
+        case "k":
+            audio= new Audio("./sounds/tom-3.mp3");
+            audio.play();                   
+            break;
+        case "l":
+            audio= new Audio("./sounds/tom-4.mp3");
+            audio.play();                   
+            break;                                                   
+        default:
+            console.log(buttonInnerHTML);
+            break;
+    }
+}
+
+
+for(var i=0; i<document.querySelectorAll(".drum").length;i++)
+    {
+        document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+            var buttonInnerHTML = this.innerHTML;
+            wordChecker(buttonInnerHTML);
+            buttonAnimation(buttonInnerHTML);
+        });
+    }
+
+
+
+document.addEventListener("keydown",function(event){
+    wordChecker(event.key);
+    buttonAnimation(event.key);
+})
+
+
+function buttonAnimation(key){
+    var activeKey=document.querySelector("."+key);
+    activeKey.classList.add("pressed");
+    setTimeout(function (){activeKey.classList.remove("pressed");},100);
+   
+}
+
